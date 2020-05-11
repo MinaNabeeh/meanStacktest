344d56f81abe53877121112a262cd574b2a94690
unknown <miina.nabeeh@gmail.com>
2020-05-11 04:18:47 +0200

    backend and front end

---
+++ nodeDemo/client/src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { AppHttpService } from './app-http.service';
+import { RegisterModel } from '../models/register.model';
+import { LoginModel } from '../models/login.model';
+
+
+
+@Injectable()
+export class AuthService {
+
+  constructor(private appHttp: AppHttpService) { }
+
+  registerUser(user: RegisterModel) {
+    return this.appHttp.post('users/register', user)
+    .map(res => res.json());
+  }
+
+  authenticateUser(user: LoginModel) {
+    return this.appHttp.post('users/login', user)
+    .map(res => res.json());
+  }
+
+}
